87e060d5b3d6dd01a74ec61c40879fad234cd823
Sonia-Chalouah <votre@email.com>
2024-07-09 11:49:33 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './shared/notfound/notfound.component';
+import { HomeComponent } from './shared/home/home.component';
+import { SignupComponent } from './basic/components/signup/signup.component';
+import { LoginComponent } from './basic/components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'client', loadChildren: () => import('./client/client.module').then(m => m.ClientModule) }, 
+  { path: 'company', loadChildren: () => import('./company/company.module').then(m => m.CompanyModule) },
+  { path: '**', component: NotfoundComponent }  // Notez que cette route doit être placée en dernier
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './shared/home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LoginComponent } from './basic/components/login/login.component';
+import { SignupClientComponent } from './basic/components/signup-client/signup-client.component';
+import { ClientDashboardComponent } from './client-dashboard/client-dashboard.component';
+import { CompanyDashboardComponent } from './company-dashboard/company-dashboard.component';
+import { NotfoundComponent } from './shared/notfound/notfound.component';
+import { SignupComponent } from './basic/components/signup/signup.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupClientComponent,
+    ClientDashboardComponent,
+    CompanyDashboardComponent,
+    NotfoundComponent,
+    SignupComponent,
+   
+  
   ],
   imports: [
     BrowserModule,
